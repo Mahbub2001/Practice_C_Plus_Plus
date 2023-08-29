70b4f13b5e47bea8c80e8e441b51ea8ea072f30e
Mahbub2001 <imahbubturza@gmail.com>
2023-08-29 10:16:12 +0600

    update

---
+++ union_by_rank.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n";
+
+int parent[1000];
+int parentLevel[1000];
+
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parentLevel[i] = 0;
+    }
+}
+
+int dsu_find(int node)
+{
+    while (parent[node] != - 1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+
+void dsu_union(int a, int b)
+{
+    int leaderA = dsu_find(a);
+    int leaderB = dsu_find(b);
+    if (leaderA != leaderB)
+    {
+        if (parentLevel[leaderA] > parentLevel[leaderB])
+        {
+            parent[leaderB] = leaderB;
+        }
+        else if (parentLevel[leaderB] > parentLevel[leaderA])
+        {
+            parent[leaderA] = leaderB;
+        }
+        else
+        {
+            parent[leaderB] = leaderA;
+            parentLevel[leaderA]++;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, e;
+    cin >> n >> e;
+    dsu_set(n);
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        dsu_union(a, b);
+    }
+
+    cout<<dsu_find(5);
+
+    return 0;
+}
+
+/*
+7 4
+1 2
+2 3
+4 5
+6 5
+
+
+*/
